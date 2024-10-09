03b0c9ee11c30fe3b9abd93ccd171f87efd1aa79
j0haz <125370073+j0haz@users.noreply.github.com>
2024-10-09 14:01:51 +0200

    Update record_browser_session.sh

--- record_browser_session.sh
+++ record_browser_session.sh
@@ -8,5 +8,7 @@ SSLKEYLOGFILE="$sslkeylogfile" firefox "$URL" & # launch firefox as subprocess
 
 # capturing traffic and save to 'recording.pcap'
 # the option 'tls.keylog_file' tells tshark where to find the logged TLS session keys
+# you must configure wireshark/tshark accordingly if you want to allow packet capturing for non-root users
+
 tshark -o "tls.keylog_file:$sslkeylogfile" -w recording.pcap
 
